209e0ded9ff4d2020b07fabc002b78c992dbbd09
MitchellBarure <m.barure@alustudent.com>
2025-06-27 14:41:25 -0700

    Almost done

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-word = "Holberton"; print(f"First 3 letters: {word[:3]}\nLast 2 letters: {word[-2:]}\nMiddle word: {word[1:-1]}")
+word = "Holberton"
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+print("First 3 letters:", word_first_3)
+print("Last 2 letters:", word_last_2)
+print("Middle word:", middle_word)
--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-# concatenation without string literals
-str = str[39:66] + str[6] + str[72:76] + str[6] + str[:6]
+str = "Python is an interpreted, interactive, object-oriented programming\
+        language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[65:66] + str[77:78] + str[6:12]
 print(str)
