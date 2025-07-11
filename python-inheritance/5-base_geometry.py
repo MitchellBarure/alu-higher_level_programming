706dc424cd98e82f66426c5b35dac409f2ea5021
MitchellBarure <m.barure@alustudent.com>
2025-07-11 03:17:11 -0700

    Square#1

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class .
+"""
+
+
+class BaseGeometry:
+    """An empty class for geometry operations."""
+    pass
---
+++ python-inheritance/README.md
@@ -0,0 +1,1 @@
+This project is focused on different Class types, Inheritance, nad built-in functions such as isinstance, issubclass, type and super
