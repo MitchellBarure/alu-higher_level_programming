a16ce473eb657af4649d0473fff7413a0b028cc6
MitchellBarure <m.barure@alustudent.com>
2025-07-14 12:58:11 -0700

    Json to Object

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """
-This module contains a function that appends a string to         the end of a text file (UTF8)
+This module contains a function that appends a string to \
+        the end of a text file (UTF8)
 and returns the number of characters added.
 """
 
 
 def append_write(filename="", text=""):
     """
-    Appends a string at the end of a text file (UTF8) and             returns the number of characters added.
+    Appends a string at the end of a text file (UTF8) and \
+            returns the number of characters added.
 
     Args:
         filename (str): The name of the file to append to.
--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This module contains a function that returns the JSON representation         of an object (string).
+This module contains a function that returns the JSON representation \
+        of an object (string).
 """
 
 import json
