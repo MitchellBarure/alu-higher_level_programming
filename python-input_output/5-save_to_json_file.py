77c537d34b471a3518b0bad5105b6d9340d28391
MitchellBarure <m.barure@alustudent.com>
2025-07-14 13:15:05 -0700

    Class to Json

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This module contains a function that writes an Object to a         text file, using a JSON representation.
+This module contains a function that writes an Object to a \
+        text file, using a JSON representation.
 """
 
 import json
