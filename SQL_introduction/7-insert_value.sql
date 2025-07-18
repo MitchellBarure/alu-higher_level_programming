24c6e4bd03f32e78fd5118a5d3082b959cf9c4da
MitchellBarure <m.barure@alustudent.com>
2025-07-18 07:59:09 -0700

    SQL

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- This script inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
+
+INSERT INTO  (uid=0(root) gid=0(root) groups=0(root), ) VALUES (89, "Best School");
---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- This script displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server
+
+SELECT COUNT(id) FROM first_table WHERE uid=0(root) gid=0(root) groups=0(root) = '89';
